0a968f82b0be650410dc23c76aae222df1fe5553
pefish <darrondarron@qq.com>
2025-02-12 16:37:21 +0800

    v1.1.5

---
+++ pkg/persistence/gob.go
@@ -0,0 +1,63 @@
+package persistence
+
+import (
+	"encoding/gob"
+	"os"
+	"path"
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+)
+
+func SaveToDisk(filename string, m *sync.Map) error {
+	tempMap := make(map[string]interface{})
+	m.Range(func(key, value any) bool {
+		tempMap[key.(string)] = value
+		return true
+	})
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(tempMap)
+
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+
+	file.Sync()
+	return nil
+}
+
+func LoadFromDisk(filename string, m *sync.Map) error {
+	err := os.MkdirAll(path.Dir(filename), 0755)
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+	defer file.Close()
+
+	tempMap := make(map[string]interface{})
+	decoder := gob.NewDecoder(file)
+	err = decoder.Decode(&tempMap)
+	if err != nil {
+		if strings.Contains(err.Error(), "EOF") {
+			return nil
+		}
+		return errors.Wrap(err, "")
+	}
+
+	for key, value := range tempMap {
+		m.Store(key, value)
+	}
+	return nil
+}
